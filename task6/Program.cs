0cf5d438568c90416fd37c7e571e1fc3fa7b2ae8
alexidi <alex.vyrlan@gmail.com>
2024-03-05 18:51:39 +0200

    Add program duble array

---
+++ task6/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(5, 8);
+PrintMatrix(matrix);
