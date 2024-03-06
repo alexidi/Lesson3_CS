c2d5327842cf688420ff5e453a4d8be29d81bfac
alexidi <alex.vyrlan@gmail.com>
2024-03-06 23:28:18 +0200

    Add string row

---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива. 
+
+
+string input = Console.ReadLine()!;
+char[] MasChar = new char[input.Length];
+
+for (int i = 0; i < input.Length; i++)
+{
+    MasChar[i] = input[i];
+}
+
+void PrintMasChar(char[] MasChar)
+{
+    foreach (char i in MasChar)
+    {
+        Console.WriteLine(i);
+    }
+}
+PrintMasChar(MasChar);
