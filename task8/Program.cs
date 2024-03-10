7eaf21e3978530e5c54b3c62ca56c0c30ce16b8e
alexidi <alex.vyrlan@gmail.com>
2024-03-10 17:38:29 +0200

    Add random array

---
+++ task8/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] Random_array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 20);
+    }
+    return array;
+}
+
+void Print_array(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+    Console.WriteLine();
+
+
+
+    Console.WriteLine("Введите размер массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] array = Random_array(new int[n]);
+
+    Print_array(array);
